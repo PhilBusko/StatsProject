746b6a1cac2cbb413312fccd52a5aa2181c102a6
Phil Busko <philbert@Philberts-MacBook-Pro.local>
2019-10-07 10:52:57 -0400

    refactored lookup code

--- crime_helper.py
+++ crime_helper.py
@@ -15,30 +15,38 @@ CONSTANTS
 BASE_URL = 'https://api.usa.gov/crime/fbi/sapi/'
 API_KEY = 'nRdG8cJGUQdri4Ki1skMMeI1J4aZD0K8fvxcxC5X' 
 
-# dict or list of agencies, provides agency ORI
 LOOKUP_AGENCIES = '/api/agencies'                                        
 LOOKUP_AGENCIES_LIST = '/api/agencies/list'
 LOOKUP_AGENCIES_STATE = '/api/agencies/byStateAbbr/raw/{stateAbbr}'     
-
-# region property of agency
 LOOKUP_REGIONS = '/api/regions'
 LOOKUP_REGION_PROPS = '/api/regions/{regionName}'
-
-# state property of agency
 LOOKUP_STATES = '/api/states'
 LOOKUP_STATE_PROPS = '/api/states/{stateAbbr}'
 
-# summary per agency
 SUMMARY_AGENCY = '/api/summarized/agencies/{ori}/offenses/{since}/{until}'
 SUMMARY_AGENCY_OFFENSE = '/api/summarized/agencies/{ori}/{offense}/{since}/{until}'
 SUMMARY_STATE_OFFENSE = '/api/summarized/state/{stateAbbr}/{offense}/{since}/{until}'
 
-# offense-tkm
-OFFENSE_AGENCY = '/api/nibrs/{offense}/offense/agencies/{ori}/{variable}'
-OFFENSE_NATIONAL = '/api/nibrs/{offense}/offense/national/{variable}'
-OFFENSE_REGION = '/api/nibrs/{offense}/offense/regions/{regionName}/{variable}'
-OFFENSE_STATE = '/api/nibrs/{offense}/offense/states/{stateAbbr}/{variable}'
+OFFENSE_AGENCY = '/api/nibrs/{offense}/offense/agencies/{ori}/count'
+OFFENSE_NATIONAL = '/api/nibrs/{offense}/offense/national/count'
+OFFENSE_REGION = '/api/nibrs/{offense}/offense/regions/{regionName}/count'
+OFFENSE_STATE = '/api/nibrs/{offense}/offense/states/{stateAbbr}/count'
 
+EMPLOYMENT_AGENCY = '/api/police-employment/agencies/{ori}/{since}/{until}'
+EMPLOYMENT_YEAR = '/api/police-employment/dl/agency/{year}'
+EMPLOYMENT_NATIONAL =  '/api/police-employment/national/{since}/{until}'
+EMPLOYMENT_REGION = '/api/police-employment/regions/{regionName}/{since}/{until}'
+EMPLOYMENT_STATE = '/api/police-employment/states/{stateAbbr}/{since}/{until}'
+
+VICTIM_AGENCY = '/api/data/nibrs/{offense}/victim/agencies/{ori}/{variable}'
+VICTIM_NATIONAL = '/api/data/nibrs/{offense}/victim/national/{variable}'
+VICTIM_REGION = '/api/data/nibrs/{offense}/victim/regions/{regionName}/{variable}'
+VICTIM_STATE = '/api/data/nibrs/{offense}/victim/states/{stateAbbr}/{variable}'
+
+OFFENDER_AGENCY = '/api/data/nibrs/{offense}/offender/agencies/{ori}/{variable}'
+OFFENDER_NATIONAL = '/api/data/nibrs/{offense}/offender/national/{variable}'
+OFFENDER_REGION = '/api/data/nibrs/{offense}/offender/regions/{regionName}/{variable}'
+OFFENDER_STATE = '/api/data/nibrs/{offense}/offender/states/{stateAbbr}/{variable}'
 
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -46,13 +54,15 @@ FUNCTIONS
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 
-# get an endpoint that is NOT paginated
-def get_json(endpoint, endpoint_config={}, add_params={}, paginated=False):
+# entry point for getting data from api
+def get_json(endpoint, endpoint_config={}, add_params={}):
 
     endpoint_url = f'{BASE_URL}{endpoint}'
 
     for key, val in endpoint_config.items():
         endpoint_url = endpoint_url.replace(key, val)
+
+    print(endpoint_url)
 
     params = {
         'api_key': API_KEY
@@ -67,25 +77,26 @@ def get_json(endpoint, endpoint_config={}, add_params={}, paginated=False):
         print(f'{endpoint_url} | {params})')
         raise ex
 
+    response_json = response.json()
+
     # if the endpoint is not paginated return the current result
 
-    if not paginated:
-        return response.json()
+    if 'pagination' not in response_json:
+        return response_json
 
     # if the endpoint is paginated, aggregate the results 
-
-    else:
-        return get_paginated(endpoint_url, response)
-
-
-def get_paginated(endpoint_url, paginated_response)
-
-    # a paginated response has a wrapper dict for pagination info
     # response = {'results': [...], 'pagination': [...] }
 
-    pagination = paginated_response.json().get('pagination')
-    print(pagination)
-    max_pages = math.ceil( pagination.get('count') / pagination.get('per_page') )
+    else:
+        pagination = response_json.get('pagination')
+        print(pagination)
+        return get_paginated(endpoint_url, params, pagination)
+
+
+# called by get_json when endpoint type is paginated
+def get_paginated(endpoint_url, params, pagination):
+
+    max_pages = pagination.get('pages')
     response_ls = []
 
     for page_no in range(max_pages):
